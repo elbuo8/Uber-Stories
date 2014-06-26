0855ce42bc93fbf62381a544622008011e4ceca1
elbuo8 <yamil.asusta@sendgrid.com>
2014-06-26 13:48:17 -0600

    Stories convenience methods

--- controllers/story.go
+++ controllers/story.go
@@ -9,17 +9,16 @@ import (
 	"github.com/gorilla/mux"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
-	"log"
 	"net/http"
+	"net/mail"
+	"strconv"
+	"time"
 )
 
 func StoryHandler(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.Method == "GET":
-		vars := mux.Vars(r)
-		if user := vars["user"]; user != "" {
-			GetUserStories(w, r, user)
-		}
+		GetStories(w, r)
 	case r.Method == "PUT":
 		SubmitAPI(w, r)
 	default:
@@ -55,11 +54,70 @@ func SubmitAPI(w http.ResponseWriter, r *http.Request) {
 	ServeJSON(w, r, &Response{"id": story.ID}, http.StatusCreated)
 }
 
-func GetUserStories(w http.ResponseWriter, r *http.Request, username string) {
+func SubmitEmail(w http.ResponseWriter, r *http.Request) {
 	dbSession, ok := context.GetOk(r, "dbSession")
 	if !ok {
-		ISR(w, r)
-		log.Println("Couldn't retrive dbSession from context")
+		ISR(w, r, errors.New("Couldn't obtain DB Session"))
+		return
+	}
+	email, err := mail.ParseAddress(r.FormValue("from"))
+	if err != nil {
+		ISR(w, r, err)
+		return
+	}
+	if email.Address == "" {
+		BR(w, r, errors.New("Missing From Address"), http.StatusBadRequest)
+		return
+	}
+	if r.FormValue("text") == "" {
+		BR(w, r, errors.New("Missing Story"), http.StatusBadRequest)
+		return
+	}
+	user, errM := models.FindUserByEmail(dbSession.(*mgo.Session), email.Address)
+	if errM != nil {
+		HandleModelError(w, r, errM)
+	}
+	story, errM := models.NewStory(dbSession.(*mgo.Session), user, r.FormValue("text"))
+	if errM != nil {
+		HandleModelError(w, r, errM)
+	}
+	ServeJSON(w, r, &Response{"id": story.ID}, http.StatusCreated)
+}
+
+func GetStories(w http.ResponseWriter, r *http.Request) {
+	dbSession, ok := context.GetOk(r, "dbSession")
+	if !ok {
+		ISR(w, r, errors.New("Couldn't obtain DB Session"))
+		return
+	}
+	timeS := r.URL.Query().Get("time")
+	var timeV int64
+	var err error
+	if timeS == "" {
+		timeV = time.Now().Unix()
+	} else {
+		timeV, err = strconv.ParseInt(timeS, 0, 64)
+	}
+	if err != nil {
+		ISR(w, r, err)
+		return
+	}
+	stories, errM := models.GetStories(dbSession.(*mgo.Session), timeV)
+	if errM != nil {
+		HandleModelError(w, r, errM)
+	}
+	b, _ := json.Marshal(stories)
+	var parse []Response
+	json.Unmarshal(b, &parse)
+	ServeJSON(w, r, &Response{"stories": parse}, http.StatusOK)
+}
+
+func GetUserStories(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	username := vars["user"]
+	dbSession, ok := context.GetOk(r, "dbSession")
+	if !ok {
+		ISR(w, r, errors.New("Couldn't obtain DB Session"))
 		return
 	}
 	stories, errM := models.StoriesByUser(dbSession.(*mgo.Session), username)
