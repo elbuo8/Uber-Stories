825539b89367eac1208e68a1d2619b4fbd9fc975
elbuo8 <yamil.asusta@sendgrid.com>
2014-06-26 13:47:01 -0600

    GetStories and new Time Schema

--- models/story.go
+++ models/story.go
@@ -10,12 +10,12 @@ type Story struct {
 	ID      bson.ObjectId `bson:"_id" json:"-"`
 	Author  *User         `bson:"a" json:"owner"`
 	Content string        `bson:"cnt" json:"story"`
-	Created time.Time     `bson:"time" json:"createdAt"`
+	Created int64         `bson:"time" json:"createdAt"`
 }
 
 func NewStory(s *mgo.Session, a *User, content string) (*Story, *Error) {
 	sC := s.DB("uber-stories").C("story")
-	story := &Story{ID: bson.NewObjectId(), Author: a, Content: content, Created: time.Now()}
+	story := &Story{ID: bson.NewObjectId(), Author: a, Content: content, Created: time.Now().Unix()}
 	err := sC.Insert(story)
 	if err != nil {
 		return nil, &Error{Reason: err, Internal: true}
@@ -27,6 +27,17 @@ func StoriesByUser(s *mgo.Session, username string) ([]*Story, *Error) {
 	sC := s.DB("uber-stories").C("story")
 	var stories []*Story
 	err := sC.Find(bson.M{"a._u": username}).All(&stories)
+	if err != nil {
+		return nil, &Error{Reason: err, Internal: true}
+	}
+	return stories, nil
+}
+
+func GetStories(s *mgo.Session, t int64) ([]*Story, *Error) {
+	sC := s.DB("uber-stories").C("story")
+	var stories []*Story
+	err := sC.Find(bson.M{"time": bson.M{"$lte": t}}).Sort("Created").Limit(30).All(&stories)
+
 	if err != nil {
 		return nil, &Error{Reason: err, Internal: true}
 	}
